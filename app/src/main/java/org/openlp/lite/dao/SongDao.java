4bc380048156cd87657b682351ad962a64928dab
Madasamy <swotge@gmail.com>
2014-10-09 21:28:43 +0800

    Load values from database

--- app/src/main/java/org/openlp/lite/dao/SongDao.java
+++ app/src/main/java/org/openlp/lite/dao/SongDao.java
@@ -15,55 +15,61 @@ import java.util.List;
 /**
  * Created by Madasamy on 8/10/2014.
  */
-public class SongDao {
-
+public class SongDao
+{
     private SQLiteDatabase database;
     private OpenlpliteHelper dbHelper;
-    private String[] allColumns = { OpenlpliteHelper.COLUMN_ID,
-            OpenlpliteHelper.COLUMN_COMMENT };
+    private String[] allColumns = {OpenlpliteHelper.COLUMN_ID,
+            OpenlpliteHelper.COLUMN_SONG};
 
-    public SongDao(Context context) {
+    public SongDao(Context context)
+    {
         dbHelper = new OpenlpliteHelper(context);
     }
 
-    public void open() throws SQLException {
+    public void open() throws SQLException
+    {
         database = dbHelper.getWritableDatabase();
     }
 
-    public void close() {
+    public void close()
+    {
         dbHelper.close();
     }
 
-    public Song createComment(String song) {
+    public Song create(String song)
+    {
         ContentValues values = new ContentValues();
-        values.put(OpenlpliteHelper.COLUMN_COMMENT, song);
-        long insertId = database.insert(OpenlpliteHelper.TABLE_COMMENTS, null,
+        values.put(OpenlpliteHelper.COLUMN_SONG, song);
+        long insertId = database.insert(OpenlpliteHelper.TABLE_SONGS, null,
                 values);
-        Cursor cursor = database.query(OpenlpliteHelper.TABLE_COMMENTS,
+        Cursor cursor = database.query(OpenlpliteHelper.TABLE_SONGS,
                 allColumns, OpenlpliteHelper.COLUMN_ID + " = " + insertId, null,
                 null, null, null);
         cursor.moveToFirst();
-        Song newComment = cursorToComment(cursor);
+        Song newSong = cursorToSong(cursor);
         cursor.close();
-        return newComment;
+        return newSong;
     }
 
-    public void deleteComment(Song song) {
+    public void delete(Song song)
+    {
         long id = song.getId();
         System.out.println("Comment deleted with id: " + id);
-        database.delete(OpenlpliteHelper.TABLE_COMMENTS, OpenlpliteHelper.COLUMN_ID
+        database.delete(OpenlpliteHelper.TABLE_SONGS, OpenlpliteHelper.COLUMN_ID
                 + " = " + id, null);
     }
 
-    public List<Song> getAllComments() {
+    public List<Song> findAll()
+    {
         List<Song> songs = new ArrayList<Song>();
 
-        Cursor cursor = database.query(OpenlpliteHelper.TABLE_COMMENTS,
+        Cursor cursor = database.query(OpenlpliteHelper.TABLE_SONGS,
                 allColumns, null, null, null, null, null);
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            Song song = cursorToComment(cursor);
+            Song song = cursorToSong(cursor);
             songs.add(song);
             cursor.moveToNext();
         }
@@ -72,7 +78,8 @@ public class SongDao {
         return songs;
     }
 
-    private Song cursorToComment(Cursor cursor) {
+    private Song cursorToSong(Cursor cursor)
+    {
         Song comment = new Song();
         comment.setId(cursor.getLong(0));
         comment.setComment(cursor.getString(1));
--- app/src/main/java/org/openlp/lite/helper/OpenlpliteHelper.java
+++ app/src/main/java/org/openlp/lite/helper/OpenlpliteHelper.java
@@ -10,17 +10,17 @@ import android.util.Log;
  */
 public class OpenlpliteHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_COMMENTS = "comments";
+    public static final String TABLE_SONGS = "songs";
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_COMMENT = "comment";
+    public static final String COLUMN_SONG = "song";
 
-    private static final String DATABASE_NAME = "commments.db";
+    private static final String DATABASE_NAME = "openlplite.db";
     private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
-            + TABLE_COMMENTS + "(" + COLUMN_ID
-            + " integer primary key autoincrement, " + COLUMN_COMMENT
+            + TABLE_SONGS + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_SONG
             + " text not null);";
 
     public OpenlpliteHelper(Context context) {
@@ -37,7 +37,7 @@ public class OpenlpliteHelper extends SQLiteOpenHelper {
         Log.w(OpenlpliteHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SONGS);
         onCreate(db);
     }
 }
